70312ae1df15adc51ff34a3aff29b6a9bd967f3a
Vitaliy Ivanov <vitaliy@staffbase.com>
2018-07-12 13:53:21 +0200

    updated lib exports

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@
  * interface exports
  */
 export { canDownload as deviceCanDownload } from './lib/device';
-export { openLink as openExternalLink } from './lib/app';
+export { openLink as openLink } from './lib/app';
+export { openLinkExternal as openLinkExternal } from './lib/app';
+export { openLinkInternal as openLinkInternal } from './lib/app';
