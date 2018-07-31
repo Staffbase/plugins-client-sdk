ed8df2e0f7286ce0c9bc8bf4c2ad334c7991fa39
Daniel Große <daniel.grosse@staffbase.com>
2018-07-31 14:16:51 +0200

    Adds language normalizer

---
+++ src/lib/utils/normalize.js
@@ -0,0 +1,22 @@
+/**
+ * Handles the locale codes as the frontend
+ *
+ * @param {string} locale the locale code
+ *
+ * @return {string} the first part of the locale
+ */
+export default function normalize(locale) {
+  locale = (locale && locale.split(/-|_/)[0]) || locale; // use only first part
+
+  if (['nb', 'nn'].indexOf(locale) !== -1) {
+    // fix Norwegian language code to conform with our non-standard stuff
+    locale = 'no';
+  }
+
+  if (locale === 'iw') {
+    // fix legacy Hebrew language code from our backend to conform with our frontend
+    locale = 'he';
+  }
+
+  return locale;
+}
