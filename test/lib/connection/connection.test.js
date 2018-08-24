6d92320101e426bc4f3d9c6a4d1a8d4feee8da2f
Daniel Große <daniel.grosse@staffbase.com>
2018-08-24 15:13:16 +0200

    Changes initialization to ondemand, adds upgrade of connector

--- test/lib/connection/connection.test.js
+++ test/lib/connection/connection.test.js
@@ -1,16 +1,87 @@
 /* eslint-env jest, es6 */
 
-import sendMessage from '../../../src/lib/connection/connection.js';
+import stubPostMessage from './../mocks';
+import sendMessage, { disconnect } from '../../../src/lib/connection/connection.js';
 import command from '../../../src/lib/connection/commands.js';
+
+const mockVersion36 = '3.6';
+const mockVersion35 = '3.5';
+const standardMsg36 = [
+  'SUCCESS',
+  0,
+  {
+    platform: { native: 'ios', mobile: true, version: mockVersion36 },
+    language: {
+      branchLanguages: {
+        de: {
+          key: 'de',
+          locale: 'de_DE',
+          name: 'Deutsch',
+          localizedName: 'Deutsch'
+        },
+        en: {
+          key: 'en',
+          locale: 'en_US',
+          name: 'English',
+          localizedName: 'Englisch'
+        }
+      }
+    }
+  }
+];
+
+const standardMsg35 = {
+  state: 'platformInfo',
+  info: { native: 'ios', mobile: true, version: mockVersion35 }
+};
 
 describe('connection', () => {
   describe('sendMessage', () => {
-    test('should provide a working sendmesage without a connection', async () => {
+    let messageStup;
+
+    afterEach(() => {
+      disconnect();
+    });
+
+    test('should provide a working sendmessage without a connection', async () => {
+      let result = await sendMessage(command.version);
+
+      expect(result).toBe('3.4');
+    });
+
+    test('should receive a message from postmessage connector if available', async () => {
+      messageStup = stubPostMessage(standardMsg36);
+      let result = await sendMessage(command.version);
+      messageStup.stopMessaging();
+
+      expect(result).toBe(mockVersion36);
+    });
+
+    test('should receive a message from postmessage legacy connector if available', async () => {
+      messageStup = stubPostMessage(standardMsg35);
+      let result = await sendMessage(command.version);
+      messageStup.stopMessaging();
+
+      expect(result).toBe(mockVersion35);
+    });
+
+    test('should upgrade a connection if the postmessage takes longer than the fallback timeout', async () => {
+      messageStup = stubPostMessage(standardMsg36, 600);
+
+      window.setTimeout(() => {});
       let result = await sendMessage(command.version);
       expect(result).toBe('3.4');
+      await delay(100);
+
+      result = await sendMessage(command.version);
+      messageStup.stopMessaging();
+
+      expect(result).toBe(mockVersion36);
     });
   });
 });
+
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
 
 /*
  * Mock the post message interface
