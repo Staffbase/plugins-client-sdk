4e6f615d871930b58b4e56cf99b06d0068c9c4b2
Daniel Große <daniel.grosse@staffbase.com>
2021-05-27 07:51:39 +0200

    chore: clean up dependencies
    - remove unnecessary dependencies
    - reconfigure husky and commit lint

--- rollup.dev.js
+++ rollup.dev.js
@@ -1,32 +1,11 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
-import stripLogger from 'rollup-plugin-strip-logger';
-import license from 'rollup-plugin-license';
-import pkg from './package.json';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 import html from 'rollup-plugin-template-html';
-
-
-const bannerTemplate = `
-Bundle of <%= pkg.name %>
-@file <%= pkg.description %>
-@see <%= pkg.homepage %>
-@version <%= pkg.version %>
-
-<% _.forEach(pkg.contributors, function (contributor) { %>@author <%= contributor %>
-<% }) %>
-@copyright <%= pkg.author.name %> <%= moment().format('YYYY') %>
-@license <%= pkg.license %>
-`;
+import pkg from './package.json';
 
 const defaultBrowserPluginOptions = [
-    // stripLogger({
-    //   variableNames: ['log'],
-    //   propertyNames: ['debug', 'enableAll'],
-    //   packageNames: ['log-level']
-    // }),
     resolve(),
     commonjs(),
     babel({
